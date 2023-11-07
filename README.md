653106000acd90f040a62b061fd99cd03f93b4fa
Wladimir Palant <fqcgithub@palant.de>
2023-11-07 12:16:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,174 @@
+# Integrated Nodes for ComfyUI
+
+This tool will turn entire workflows or parts of them into single integrated nodes. In a way, it is similar to the Node Templates functionality but hides the inner structure. This is useful if all you want is to reuse and quickly configure a bunch of nodes without caring how they are interconnected.
+
+## Installation
+
+To install, clone this repository into `ComfyUI/custom_nodes` folder with `git clone https://github.com/palant/integrated-nodes-comfyui`. Edit the example `integrated_nodes.yaml` configuration file as you see fit. After that, restart ComfyUI.
+
+## Example nodes
+
+The default `integrated_nodes.yaml` contains a bunch of nodes which you might consider useful.
+
+### Double Prompt
+
+This combines two CLIP encoding prompts in a single node:
+
+![A ComfyUI node titled Double Prompt containing two text widgets](double_prompt.png)
+
+It is equivalent to the following workflow elements:
+
+![A ComfyUI workflow containing two identical nodes titled CLIP Text Encode (Prompt)](double_prompt_workflow.png)
+
+### ControlNet with Prompts
+
+This combines an Apply ControlNet node with the nodes typically used with it: ControlNet loader, two text prompts and an image loader.
+
+![A ComfyUI node titled ControlNet with Prompts containing two text widgets, followed by the widgets control_net_name, strength, start_percent, ent_percent, image](controlnet_prompts.png)
+
+It is equivalent to the following workflow elements:
+
+![A ComfyUI workflow containing two identical nodes titled CLIP Text Encode (Prompt), Load ControlNet Model node, Load Image node, all connected to an Apply ControlNet (Advanced) node](controlnet_prompts_workflow.png)
+
+### txt2img
+
+This is a complete text to image workflow in a single node:
+
+![A ComfyUI node titled txt2img containing the widget ckpt_name followed by two text prompts, then the widgets seed, control_after_generate, steps, cfg, sampler_name, scheduler, denoise, width, height, batch_size, filename_prefix, file_type](txt2img.png)
+
+It is equivalent to the following workflow:
+
+![A ComfyUI workflow containing the nodes Load Checkpoint, twice CLIP Text Encode (Prompt), KSampler, Empty Latent Image, VAE Decode, Save Image](txt2img_workflow.png)
+
+### Inpaint
+
+This is a complete image to image workflow in a single node:
+
+![A ComfyUI node titled Inpaint containing the widget ckpt_name followed by two text prompts, then the widgets seed, control_after_generate, steps, cfg, sampler_name, scheduler, denoise, image, grow_mask_by, filename_prefix, file_type](inpaint.png)
+
+It is equivalent to the following workflow:
+
+![A ComfyUI workflow containing the nodes Load Checkpoint, twice CLIP Text Encode (Prompt), Load Image, VAE Encode (for Inpainting), KSampler, VAE Decode, Save Image](inpaint_workflow.png)
+
+### Inpaint not Masked
+
+This is largely identical to the Inpaint node but inverts the image mask, so that the non-masked areas are inpainted:
+
+![A ComfyUI node titled Inpaint not Masked containing the widget ckpt_name followed by two text prompts, then the widgets seed, control_after_generate, steps, cfg, sampler_name, scheduler, denoise, image, grow_mask_by, filename_prefix, file_type](inpaint_not_masked.png)
+
+It is equivalent to the following workflow:
+
+![A ComfyUI workflow containing the nodes Load Checkpoint, twice CLIP Text Encode (Prompt), Load Image, VAE Encode (for Inpainting), KSampler, VAE Decode, Save Image](inpaint_not_masked_workflow.png)
+
+## Adding your own integrated nodes
+
+You can add edit `integrated_nodes.yaml` file and add your own entries. These look like this:
+
+```yaml
+DoublePrompt:
+  display_name: Double Prompt
+  merge_inputs:
+    clip: clip_2
+  rename_outputs:
+    1 CONDITIONING: positive
+    2 CONDITIONING: negative
+  workflow: double_prompt.json
+```
+
+You put the node’s internal name first and then list its properties indented. Supported properties are explained below.
+
+### workflow
+
+This is the only required property, the path to the workflow JSON files. You can provide both absolute and relative paths. The latter are resolved relative to the directory containing the `integrated_nodes.yaml` file.
+
+The JSON file can contain either a saved workflow or an exported node template. If a node template file contains multiple templates, only the first one will be used.
+
+While any workflow file should work out of the box, in some cases you may want to edit it. Any values entered into the workflow’s widgets will become the new defaults for the integrated node. If you would rather keep ComfyUI’s defaults instead, remove the `widget_values` properties from the workflow file.
+
+The order in which the widgets show up in the integrated node is determined by the `order` property of the nodes in the original workflow. If you prefer to have the widgets of a different node at the top, adjust its `order` property accordingly.
+
+### display_name
+
+This is the name under which the node should be displayed in ComfyUI. If omitted, the node’s internal name will be used.
+
+Unlike the internal name, the display name can be changed freely without invalidating existing workflows.
+
+### merge_inputs
+
+All inputs of the original workflow that don’t have links attached to them will be present in the integrated node. Inputs with the same type can be merged via `merge_inputs` property however.
+
+For example, if the original workflow contained two prompt widgets, both had a `clip` input. In the integrated mode, the second `clip` input is automatically renamed into `clip_2`. Typically however, both are supposed to receive the same CLIP model. So `clip_2` input can be merged into the `clip` input:
+
+```yaml
+  merge_inputs:
+    clip: clip_2
+```
+
+It is also possible to merge more than two inputs by taking advantage of YAML’s list syntax:
+
+```yaml
+  merge_inputs:
+    clip:
+    - clip_2
+    - clip_3
+    - clip_4
+```
+
+The same can be written on one line if desirable:
+
+```yaml
+  merge_inputs:
+    clip: [clip_2, clip_3, clip_4]
+```
+
+### rename_inputs
+
+When multiple inputs of the original workflow have the same name, the names are automatically adjusted by appending numbers to them. For example, the first input named `model` will keep its name while the second will be renamed into `model_2`, the third into `model_3` and so on.
+
+The inputs can be given more meaningful names via `rename_inputs` property, for example:
+
+```yaml
+  rename_inputs:
+    model: initial_model
+    model_2: refiner_model
+```
+
+### export_outputs
+
+By default, any outputs of the original workflow not linked to anything will show up on the integrated node. This behavior might not always be desirable, if an output should show up despite being used internally as well, or if an output needs to be ignored.
+
+The `export_outputs` allows listing explicitly which outputs should show up on the integrated node:
+
+```yaml
+  export_outputs:
+  - 5 positive
+  - 5 negative
+```
+
+This instructs the tool to export the outputs named `positive` and `negative` of the node with ID `5`.
+
+*Note*: While workflow JSON files have the node ID specified explicitly in the data, exported node templates do not. Here the node ID is the index of a node in the `nodes` list, with the first node having the ID `0`.
+
+### rename_outputs
+
+By default, all outputs keep the name they had on the original nodes. This can be undesirable, e.g. if multiple nodes have outputs with the same name. The `rename_outputs` property allows them to be renamed:
+
+```yaml
+  rename_outputs:
+    1 CONDITIONING: positive
+    2 CONDITIONING: negative
+```
+
+This instructs the tool to rename the output named `CONDITIONING` on the node with ID `1` into `positive` and the output with the same name on the node with ID `2` into `negative`.
+
+*Note*: While workflow JSON files have the node ID specified explicitly in the data, exported node templates do not. Here the node ID is the index of a node in the `nodes` list, with the first node having the ID `0`.
+
+## Caveats
+
+This is a purely server-side approach, it has no impact on special functionality added on the client side. This means that nodes like Primitive or Reroute cannot be integrated, these have no server-side equivalents. Also, setting a default value for the `control_after_generate` widget isn’t possible.
+
+ComfyUI’s image sizing approach appears to be flawed. As a consequence, images on complicated nodes will appear too small and might be hidden entirely when the node is added initially.
+
+ComfyUI currently doesn’t support multiple images on a single node. So if the Inpaint node for example contains both the input and the output image, only one of these images will be displayed at a time (whichever changed last).
+
+The workflow JSON format saves widget values as a plain list, without their names. This approach relies on the widgets to be always present in the same order. If future changes to ComfyUI cause the node’s widgets to be presented in a different order, the defaults from the original workflow might get applied to the wrong widgets.
---
+++ integrated_nodes.py
@@ -0,0 +1,502 @@
+import json
+import os
+import sys
+import traceback
+import yaml
+
+from nodes import NODE_CLASS_MAPPINGS as GLOBAL_NODE_CLASS_MAPPINGS
+
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
+
+class Node(object):
+    def __init__(self, id, workflow, exported_inputs):
+        type = workflow["type"]
+        cls = GLOBAL_NODE_CLASS_MAPPINGS.get(type)
+        if cls is None:
+            raise Exception(f"Unknown node type {type}")
+
+        self.id = id
+        self.cls = cls
+        self.input_map = {}
+        self.inputs = []
+        self.input_slots = []
+        self.outputs = []
+        self.output_types = []
+        self.output_names = []
+        self.workflow = workflow
+
+        types = getattr(self.cls, "RETURN_TYPES", ())
+        names = getattr(self.cls, "RETURN_NAMES", types)
+        for type, name in zip(types, names):
+            self.outputs.append([])
+            self.output_types.append(type)
+            self.output_names.append(name)
+
+        inputs = self.cls.INPUT_TYPES()
+        for name, descriptor in inputs.get("required", {}).items():
+            input = RequiredInput(register=new_register(), name=name, descriptor=descriptor)
+            self.input_map[name] = input.register
+            self.inputs.append(input)
+        for name, descriptor in inputs.get("optional", {}).items():
+            input = OptionalInput(register=new_register(), name=name, descriptor=descriptor)
+            self.input_map[name] = input.register
+            self.inputs.append(input)
+        for name, type in inputs.get("hidden", {}).items():
+            # Hidden inputs are always exported
+            if name not in exported_inputs:
+                exported_inputs[name] = HiddenInput(register=new_register(), descriptor=type)
+            elif exported_inputs[name].type != type:
+                raise Exception(f"Mismatched types for hidden input {name}: {type} and {exported_inputs[name].type}")
+            self.input_map[name] = exported_inputs[name].register
+
+
+    @property
+    def output_node(self):
+        return hasattr(self.cls, "OUTPUT_NODE") and self.cls.OUTPUT_NODE
+
+
+    def separate_input_slots(self, names):
+        for name in names:
+            input = next(input for input in self.inputs if input.name == name)
+            self.inputs.remove(input)
+            self.input_slots.append(input)
+
+
+    def assign_defaults(self, defaults):
+        skip_next = False
+        i = 0
+        for value in defaults:
+            if skip_next:
+                skip_next = False
+                continue
+
+            input = self.inputs[i]
+            i += 1
+
+            input.set_default_value(value)
+
+            if input.type == "INT" and input.name in ("seed", "denoise_seed"):
+                # Seed inputs are special and result in two saved values, ignore next one
+                skip_next = True
+
+
+class Input(object):
+    def __init__(self, register, name, descriptor):
+        self.registers = [register]
+        self.name = name
+        self.descriptor = descriptor
+
+
+    def set_default_value(self, value):
+        if len(self.descriptor) == 1:
+            self.descriptor = (self.type, {"default": value})
+        else:
+            self.descriptor[1]["default"] = value
+
+    @property
+    def register(self):
+        return self.registers[0]
+
+
+    @property
+    def type(self):
+        return self.descriptor[0]
+
+
+class RequiredInput(Input):
+    COLLECTION = "required"
+
+
+class OptionalInput(Input):
+    COLLECTION = "optional"
+
+
+class HiddenInput(object):
+    COLLECTION = "hidden"
+
+    def __init__(self, register, descriptor):
+        self.register = register
+        self.descriptor = descriptor
+
+    @property
+    def registers(self):
+        return [self.register]
+
+
+class Output(object):
+    def __init__(self, register, name, type):
+        self.register = register
+        self.name = name
+        self.type = type
+
+
+class NodeProcessor(object):
+    # Overwritten by subclasses
+    NODE = None
+
+    def __init__(self):
+        self.inner = self.NODE.cls()
+
+
+    @classmethod
+    def map_inputs(s, state):
+        params = {}
+        for name, register in s.NODE.input_map.items():
+            params[name] = state[register]
+        return params
+
+
+    @classmethod
+    def validate(s, state):
+        if hasattr(s.NODE.cls, "VALIDATE_INPUTS"):
+            return s.NODE.cls.VALIDATE_INPUTS(**s.map_inputs(state))
+        else:
+            return True
+
+
+    @classmethod
+    def has_is_changed(s):
+        return hasattr(s.NODE.cls, "IS_CHANGED")
+
+
+    @classmethod
+    def is_changed(s, state):
+        return s.NODE.cls.IS_CHANGED(**s.map_inputs(state))
+
+
+    def process(self, state, ui):
+        function_name = self.NODE.cls.FUNCTION
+        result = getattr(self.inner, function_name)(**self.map_inputs(state))
+        if isinstance(result, dict):
+            outputs = result.get("result", ())
+            for key, value in result.get("ui", {}).items():
+                ui.setdefault(key, []).extend(value)
+        else:
+            outputs = result
+        for register_ids, value in zip(self.NODE.outputs, outputs):
+            for register_id in register_ids:
+                state[register_id] = value
+
+
+class IntegratedNode(object):
+    FUNCTION = "process"
+
+    # Overwritten by subclasses
+    PROCESSORS = None
+    INPUTS = None
+    OUTPUTS = None
+
+    def __init__(self):
+        self.processors = [processor() for processor in self.PROCESSORS]
+        pass
+
+
+    @classmethod
+    def construct_state(s, **kwargs):
+        state = {}
+        for name, value in kwargs.items():
+            try:
+                input = s.INPUTS[name]
+            except KeyError:
+                raise Exception(f"Unexpected parameter {name}")
+            for register in input.registers:
+                state[register] = value
+        return state
+
+
+    @classmethod
+    def INPUT_TYPES(s):
+        types = {}
+        for name, input in s.INPUTS.items():
+            types.setdefault(input.COLLECTION, {})[name] = input.descriptor
+        return types
+
+    @classmethod
+    @property
+    def RETURN_TYPES(s):
+        return [output.type for output in s.OUTPUTS]
+
+
+    @classmethod
+    @property
+    def RETURN_NAMES(s):
+        return [output.name for output in s.OUTPUTS]
+
+
+    @classmethod
+    def VALIDATE_INPUTS(s, **kwargs):
+        state = s.construct_state(**kwargs)
+        for processor in s.PROCESSORS:
+            validation_result = processor.validate(state)
+            if validation_result is not True:
+                return validation_result
+        return True
+
+
+    @classmethod
+    def _IS_CHANGED(s, **kwargs):
+        state = s.construct_state(**kwargs)
+        result = []
+        for processor in s.PROCESSORS:
+            if processor.has_is_changed():
+                result.append(processor.is_changed(state))
+        return "".join(result)
+
+
+    def process(self, **kwargs):
+        state = self.construct_state(**kwargs)
+        ui = {}
+        for processor in self.processors:
+            processor.process(state, ui)
+
+        result = []
+        for output in self.OUTPUTS:
+            result.append(state[output.register])
+        return {
+            "result": result,
+            "ui": ui,
+        }
+
+
+def warn(warning):
+    print(f"integrated_nodes.yaml: {warning}", file=sys.stderr)
+
+
+max_register_id = 0
+def new_register():
+    global max_register_id
+    max_register_id += 1
+    return max_register_id
+
+
+def create_nodes(workflow):
+    nodes = []
+    exported_inputs = {}
+    for id, workflow in enumerate(workflow):
+        id = workflow.get("id", id)
+        node = Node(id, workflow, exported_inputs)
+        node.separate_input_slots(map(lambda slot: slot["name"], workflow.get("inputs", [])))
+        node.assign_defaults(workflow.get("widgets_values", []))
+        nodes.append(node)
+    nodes = sorted(nodes, key=lambda node: node.workflow.get("order", 0))
+    return nodes, exported_inputs
+
+
+def connect_links(workflow, nodes):
+    def node_by_id(id):
+        return next(node for node in nodes if node.id == id)
+
+    linked_inputs = set()
+    dependencies = {}
+    for link in workflow:
+        if len(link) == 6:
+            # Workflow
+            _, from_id, from_slot, to_id, to_slot, _ = link
+        else:
+            # Node template
+            from_id, from_slot, to_id, to_slot, _ = link
+        from_node = node_by_id(from_id)
+        to_node = node_by_id(to_id)
+        dependencies.setdefault(to_node, set()).add(from_node)
+
+        input = to_node.input_slots[to_slot]
+        output_type = from_node.output_types[from_slot]
+        if input.type != output_type:
+            raise Exception(f"Cannot connect input of type {input} to output of type {output_type}")
+
+        from_node.outputs[from_slot].append(input.register)
+        linked_inputs.add(input.register)
+    return linked_inputs, dependencies
+
+
+def create_node_processor(node):
+    return type("NodeProcessor", (NodeProcessor,), {
+        "NODE": node,
+    })
+
+
+def process_workflow(workflow, export_outputs, rename_outputs):
+    if not isinstance(workflow, dict):
+        raise Exception("Workflow is not a dictionary")
+
+    nodes, exported_inputs = create_nodes(workflow.get("nodes", []))
+    linked_inputs, dependencies = connect_links(workflow.get("links", []), nodes)
+
+    exported_outputs = []
+    for node in nodes:
+        for targets, name, type in zip(node.outputs, node.output_names, node.output_types):
+            output_id = f"{node.id} {name}"
+            # Use explicitly specified exports, fall back to exporting everything not linking anywhere
+            if (export_outputs is not None and output_id in export_outputs) or (export_outputs is None and len(targets) == 0):
+                output = Output(register=new_register(), name=rename_outputs.get(output_id, name), type=type)
+                exported_outputs.append(output)
+                targets.append(output.register)
+
+        # Inputs without incoming links are exported
+        for input in node.input_slots + node.inputs:
+            if input.register in linked_inputs:
+                continue
+
+            # Find a non-conflicting name for the input
+            i = 1
+            name = input.name
+            while name in exported_inputs:
+                i += 1
+                name = f"{input.name}_{i}"
+            exported_inputs[name] = input
+
+    # Find a suitable execution order for the nodes
+    execution_order = []
+    while len(execution_order) != len(nodes):
+        for node in nodes:
+            if node in execution_order:
+                continue
+            if all(source in execution_order for source in dependencies.get(node, set())):
+                execution_order.append(node)
+                break
+        else:
+            raise Exception("Dependency loop detected")
+
+    processors = list(map(create_node_processor, execution_order))
+    is_output_node = any(node.output_node for node in nodes)
+
+    return (processors, exported_inputs, exported_outputs, is_output_node)
+
+
+def merge_inputs(inputs, mapping):
+    if not isinstance(mapping, dict):
+        warn(f"merge_inputs entry should be a dictionaty but got {mapping}, ignoring")
+        return
+
+    for target, sources in mapping.items():
+        if not isinstance(sources, list):
+            sources = [sources]
+
+        try:
+            target_input = inputs[target]
+        except KeyError:
+            warn(f"Target input {target} not found, merging skipped")
+            continue
+
+        for source in sources:
+            try:
+                source_input = inputs[source]
+            except KeyError:
+                warn(f"Source input {source} not found, merging skipped")
+                continue
+
+            if target_input.type != source_input.type:
+                warn(f"Cannot merge input {source} into {target}, type mismatch: {source_input.type} vs. {target_input.type}")
+                continue
+
+            target_input.registers += source_input.registers
+            del inputs[source]
+
+
+def rename_inputs(inputs, mapping):
+    if not isinstance(mapping, dict):
+        warn(f"rename_inputs entry should be a dictionaty but got {mapping}, ignoring")
+        return
+
+    for old, new in mapping.items():
+        if old not in inputs:
+            warn(f"Cannot rename input {old}, no input wiht this name exists")
+            continue
+
+        if new in inputs:
+            warn(f"Cannot rename input {old} to {new}, another input with this name already exists")
+            continue
+
+        inputs[new] = inputs[old]
+        del inputs[old]
+
+
+def create_integrated_node(name, info):
+    if not isinstance(info, dict):
+        warn(f"Ignoring integrated node {name}, not a dictionary")
+        return
+
+    try:
+        workflow_path = info["workflow"]
+    except KeyError:
+        warn(f"Ignoring integrated node {name}, missing required workflow entry")
+        return
+
+    try:
+        with open(os.path.join(os.path.dirname(__file__), workflow_path)) as input:
+            workflow = json.load(input)
+    except:
+        traceback.print_exc()
+        warn(f"Ignoring integrated node {name}, failed loading workflow from file {workflow_path}")
+        return
+
+    if isinstance(workflow.get("templates"), list):
+        # Got a node template file
+        templates = workflow["templates"]
+        if len(templates) == 0:
+            warn(f"Ignoring integrated node {name}, node templates file contains no templates")
+            return
+        if len(templates) > 1:
+            warn(f"Node templates file for integrated node {name} contains multiple templates, only the first one will be used")
+        template = templates[0]
+        if not isinstance(template, dict):
+            warn(f"Ignoring integrated node {name}, provided node template is not a dictionary")
+            return
+        try:
+            workflow = json.loads(template["data"])
+        except:
+            traceback.print_exc()
+            warn(f"Ignoring integrated node {name}, node template data isn't a valid JSON string")
+            return
+
+    export_outputs = info.get("export_outputs")
+    if export_outputs is not None and not isinstance(export_outputs, list):
+        warn(f"export_outputs entry should be a list but got {export_outputs}, ignoring")
+        export_outputs = None
+    if export_outputs is not None:
+        export_outputs = set(export_outputs)
+
+    rename_outputs = info.get("rename_outputs", {})
+    if not isinstance(rename_outputs, dict):
+        warn(f"rename_outputs entry should be a dictionary but got {rename_outputs}, ignoring")
+        rename_outputs = {}
+
+    try:
+        (processors, inputs, outputs, is_output_node) = process_workflow(workflow, export_outputs, rename_outputs)
+    except:
+        traceback.print_exc()
+        warn(f"Ignoring integrated node {name}, failed processing workflow")
+        return
+
+    merge_inputs(inputs, info.get("merge_inputs", {}))
+    rename_inputs(inputs, info.get("rename_inputs", {}))
+
+    cls = type(name, (IntegratedNode,), {
+        "PROCESSORS": processors,
+        "INPUTS": inputs,
+        "OUTPUTS": outputs,
+        "CATEGORY": info.get("category", "integrated"),
+        "OUTPUT_NODE": is_output_node,
+    })
+
+    if any(processor.has_is_changed() for processor in processors):
+        cls.IS_CHANGED = cls._IS_CHANGED
+
+    NODE_CLASS_MAPPINGS[name] = cls
+    NODE_DISPLAY_NAME_MAPPINGS[name] = info.get("display_name", name)
+
+
+def load_config():
+    path = os.path.join(os.path.dirname(__file__), "integrated_nodes.yaml")
+    with open(path, 'r') as input:
+        data = yaml.safe_load(input)
+
+    if not isinstance(data, dict):
+        warn("File does not contain a dictionary, ignoring")
+        return
+
+    for name, info in data.items():
+        create_integrated_node(name, info)
+
+load_config()
